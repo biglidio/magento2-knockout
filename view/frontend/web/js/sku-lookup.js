f364c8780e480b4d1003814d755d55effbf31556
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-10-09 16:57:06 +0200

    #6 - Bind & display text within a Knockout.js rendered template

--- view/frontend/web/js/sku-lookup.js
+++ view/frontend/web/js/sku-lookup.js
@@ -2,6 +2,15 @@ define(['uiComponent'], function(Component){
     'use strict'
 
     return Component.extend({
+        defaults: {
+            /*  Better config it inside layout file. Example:
+                <item name="config" xsi:type="array">
+                    <item name="template" xsi:type="string">Biglidio_InventoryFulfillment/sku-lookup</item>
+                </item>
+            */
+            // template: 'Biglidio_InventoryFulfillment/sku-lookup',
+            sku: '<em>ABC123</em>'
+        },
         initialize() {
             this._super();
 
