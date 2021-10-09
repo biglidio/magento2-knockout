dbc027e0043c73fa95d7efb12c9325783c91558b
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-10-09 11:39:45 +0200

    #4 - Execute code upon loading a Knockout.js ViewModel

--- view/frontend/web/js/sku-lookup.js
+++ view/frontend/web/js/sku-lookup.js
@@ -1,5 +1,11 @@
 define(['uiComponent'], function(Component){
     'use strict'
 
-    return Component;
+    return Component.extend({
+        initialize() {
+            this._super();
+
+            console.log('The skuLookup component has been loaded.');
+        }
+    });
 });
