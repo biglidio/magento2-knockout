69f9196a5c796a57ed63cd071b86a81aabdd075c
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-10-13 20:25:10 +0200

    #34 - Write a custom Knockout.js extender to validate & enforce data integrity

--- view/frontend/web/js/model/box-configurations.js
+++ view/frontend/web/js/model/box-configurations.js
@@ -1,5 +1,6 @@
 define([
-    'ko'
+    'ko',
+    'Biglidio_InventoryFulfillment/js/ko/extenders/numeric'
 ], function(
     ko
 ) {
@@ -8,17 +9,27 @@ define([
         const divisor = 139;
 
         const data = {
-            length: ko.observable(),
-            width: ko.observable(),
-            height: ko.observable(),
-            weight: ko.observable(),
-            unitsPerBox: ko.observable(),
-            numberOfBoxes: ko.observable(),
+            length: ko.observable('16').extend({numeric: true}),
+            width: ko.observable('16').extend({numeric: true}),
+            height: ko.observable('16').extend({numeric: true}),
+            weight: ko.observable('10').extend({numeric: true}),
+            unitsPerBox: ko.observable('100').extend({numeric: true}),
+            numberOfBoxes: ko.observable('1').extend({numeric: true}),
         }
 
         data.dimensionalWeight = ko.computed(() => {
             const result = data.length() * data.width() * data.height() / divisor;
             return Math.round(result * data.numberOfBoxes());
+        });
+
+        data.totalWeight = ko.computed(() => {
+            return data.numberOfBoxes() * data.weight();
+        });
+
+        data.billableWeight = ko.computed(() => {
+            return (data.totalWeight() > data.dimensionalWeight()) 
+                ? data.totalWeight() 
+                : data.dimensionalWeight();
         });
 
         return data;
